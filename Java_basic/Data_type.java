383aa96f62007c0694210359a5fc76e5a3f3d1b2
Kawya singh <136492859+Kawyasingh@users.noreply.github.com>
2025-04-02 23:15:56 +0530

    Add files via upload

---
+++ Java_basic/Data_type.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //byte - 1[-128 to 127]
+        //short - 2
+        //int - 4 1,2,3,4
+        //long - 8
+        //float - 4 1.3
+        //double - 8
+        //char - 2 ab,c,d
+        //boolean - 1 true/False
+
+        byte age = 30;
+        int phone = 1234567890;
+        long phone2 = 12345678900L;
+        float pi = 3.14F;
+        char letter = '@';
+        boolean isAdult = true;
+
+        // Non-primitive Types
+        String name = "Apu";
+
+        System.out.println(name.length());
+    }
+}
