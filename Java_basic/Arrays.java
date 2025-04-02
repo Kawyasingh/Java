d3a535917882d30b0a5865d34e8a1d6e109a570a
Kawya singh <136492859+Kawyasingh@users.noreply.github.com>
2025-04-02 23:14:42 +0530

    Add files via upload

---
+++ Java_basic/Arrays.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 30;
+        int physics = 97;
+        int chem = 98;
+        int eng = 95;
+
+//        int[] marks =  new int[3];
+//        marks[0] = 97;
+//        marks[1] = 98;
+//        marks[2] = 95;
+
+        //length
+
+       // System.out.println(marks.length);
+
+//        int[] marks ={97,98,95};
+//
+//        int[][] finalMarks = {{97,98,95},{95,95,98}};
+//        System.out.println(finalMarks[0][1]);
+
+        //casting
+
+//        double price = 100.00;
+//        double finalPrice = price +18;
+//        System.out.println(finalPrice);
+//
+//        int p =100;
+//        int fp = p + (int)18.0;
+//        System.out.println(fp);
+
+        // constants
+
+        int age = 30;
+        age  = 31;
+        age = 32;
+
+
+        final float PI = 3.14F;
+        PI = 1.1f;
+
+
+    }
+}
