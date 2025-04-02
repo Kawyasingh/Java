1ee4889ee244ac808e77e650622cfec87c8477e7
Kawya singh <136492859+Kawyasingh@users.noreply.github.com>
2025-04-02 23:17:37 +0530

    Add files via upload

---
+++ Java_basic/Strings.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Strings
+        //concatenate
+       /* String name1 = "Aman";
+        String name2 = "Aku";
+        String name3 = name1 +"and"+ name2;
+        System.out.println(name3);
+       */
+        /*charAt
+        String name = "Aman";
+        System.out.println(name.charAt((1)));
+        */
+        //replace
+        /*String name = "Aman";
+        //     String name2 =name.replace('a','b');
+        //     System.out.println(name2);
+        //     System.out.println((name));
+        */
+        //Substring
+        String name = "Aman and Akku";
+        System.out.println(name.substring(5,9));
+
+    }
+}
