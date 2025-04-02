9762632ed540f080b3200ac7af2030f69798d9bb
Kawya singh <136492859+Kawyasingh@users.noreply.github.com>
2025-04-02 23:19:49 +0530

    Add files via upload

---
+++ Java_basic/first_java_program.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //variable
+        string name = "Aman";
+        string neighbour = "Akku";
+        int age = 30;
+
+        string friend = neighbour;
+
+    }
+}
